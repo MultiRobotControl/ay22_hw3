18936dad4465fbb1bc77febb12b202f22623145a
matthewheubach <mattheubach@live.com>
2021-10-14 16:37:28 -0700

    Update turtleletterstwo.sh

--- scripts/turtleletterstwo.sh
+++ scripts/turtleletterstwo.sh
@@ -3,7 +3,7 @@ rosservice call /turtle1/teleport_absolute 0 1 0
 rosservice call /clear
 rosservice call /spawn 6 1 0 "turtle2"
 
-rosservice call /turtle1/set_pen/ 255 0 0 2 0
+rosservice call /turtle1/set_pen 255 0 0 2 0
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 5.0, 0.0]' '[0.0, 0.0, 0.0]'
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, -1.0]'
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[5.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
@@ -13,7 +13,7 @@ rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0,
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, -5.0, 0.0]' '[0.0, 0.0, 0.0]'
 
 
-rosservice call /turtle2/set_pen/ 0 255 0 2 0
+rosservice call /turtle2/set_pen 0 255 0 2 0
 rostopic pub -1 /turtle2/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, 1.25]'
 rostopic pub -1 /turtle2/cmd_vel geometry_msgs/Twist -- '[5.5, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
 rostopic pub -1 /turtle2/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, -2.5]'
@@ -23,5 +23,3 @@ rostopic pub -1 /turtle2/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0,
 rostopic pub -1 /turtle2/cmd_vel geometry_msgs/Twist -- '[-2.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
 
 
-rosservice call /turtle2/set_pen/ 0 255 0 2 0
-rosservice call /turtle1/set_pen/ 255 0 0 2 0
